061021b5e64c488306e521c51b308df96448a800
AngelZRUNAM <angelzunigareyes@gmail.com>
2016-09-05 12:36:26 -0500

    Se agregó charset

--- NewPythonProject/src/ConvertidorBinario.py
+++ NewPythonProject/src/ConvertidorBinario.py
@@ -1,31 +1,41 @@
+# -*- coding: utf-8 -*-
+""" Nombre del Programa     : Convertidor Decimal - Binario
+    Fecha de Creación       : 28/08/2016
+    Autor(s)                : Garcia Xoconostle Ivan Rafael
+                            : Zuñiga Reyes Miguel Angel
+    Descripción general     : Programa con funciones para realizar las conversiones siguientes:
+                            Decimal - Base N        (dcmlToBaseN)
+                            Decimal - Complemento 1 (decToC1)
+                            Decimal - Complemento 2 (decToC2)
+                            Binario - Decimal       (baseNToDec)
+                            Complemento 1 - Decimal (c1ToDec)
+                            Complemento 2 - Decimal (c2ToDec                          
+"""
 
-def invert(text):
-    if text == "":
-        return ""
-    else:
-        return text[-1:] + str(invert(text[:-1]))
-
+    
 def dcmlToBaseN(number, base):
     """
-    
-    """
-    return invert(dcmlToBaseNRcrsv(number, base))
-    
-def dcmlToBaseNRcrsv(number, base):
-    """ 
-    
+    Metodo recursivo para convertir un número base 10 N a una base B
+    :parameter number:  Número en base 10 a convertir
+    :parameter base:    Base a la cual se va a convertir el número decimal.
+    :return:            Número N en base B
+    Ejemplo: number = 10, base = 2
+    -> dcmlToBaseN(10/2,2) + "0" 
+    -> dcmlToBaseN(5/2,2) + "10" 
+    -> dcmlToBaseN(2/2,2) + "010" 
+    -> regresa "1010" 
     """
     if number < base:
         return number
     else:
-        return str(number % base) + str(dcmlToBaseNRcrsv(number/base,base))
+        return  str(dcmlToBaseN(number/base,base)) + str(number % base)
     
 def decToC1(numberToCnvrt, deep):
     """
     
     """
     if deep == 0:
-        numberToCnvrt = dcmlToBaseNRcrsv(numberToCnvrt,2)
+        numberToCnvrt = dcmlToBaseN(numberToCnvrt,2)
         deep = 1
         
     if numberToCnvrt == "":
@@ -75,7 +85,7 @@ def c1ToDec(textC1):
     """
     
     """
-    return baseNToDec(invert(decToC1(textC1,2, 1)),0)
+    return baseNToDec(decToC1(textC1,2, 1),0)
 
 def c2ToDec(textC2):
     """
@@ -87,4 +97,4 @@ def c2ToDec(textC2):
         return baseNToDec(textC2,2, 0)
 
     
-         
+print(dcmlToBaseN(10, 2))
